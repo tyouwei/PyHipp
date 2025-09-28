1c5cf0c4340321f8fffd7fe17507a71df623d0ba
Tham You Wei <tyouwei@Thams-MacBook-Pro.local>
2025-09-29 02:23:39 +0800

    Revert quiz 1A changes

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -14,12 +14,8 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-import time; \
 import DataProcessingTools as DPT; \
-lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
+lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*','*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
-hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
-print(time.localtime());"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:158394348457:awsnotify --message "FSJobDone"
+hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*','*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
+hfall.save();"
